68a9e75a0f62a31b0d937a82a73c15a0af3bbfc4
Spandana R <spandanaspandu7607@gmail.com>
2026-03-12 21:47:33 +0530

    Solved Two Sum

--- Arrays/1_Two_Sum.java
+++ Arrays/1_Two_Sum.java
@@ -2,9 +2,9 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
        Map<Integer,Integer> map = new HashMap<>();
        for(int i = 0 ; i < nums.length ; i++){
-        int compliment = target - nums[i];
+        int complement = target - nums[i];
         if(map.containsKey(compliment)){
-            return new int[] {map.get(compliment),i};
+            return new int[] {map.get(complement),i};
         }
         else{
             map.put(nums[i], i);
